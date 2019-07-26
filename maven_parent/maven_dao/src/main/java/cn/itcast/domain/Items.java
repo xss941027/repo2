ddbf09507ce3ab5ac82b07e9ccfe419b10aa1f19
Bruce <Bruce@163.com>
2019-07-26 18:08:48 +0800

    提交maven_parent

---
+++ maven_parent/maven_dao/src/main/java/cn/itcast/domain/Items.java
@@ -0,0 +1,72 @@
+package cn.itcast.domain;
+
+import java.util.Date;
+
+public class Items {
+    private int id;
+    private String name;
+    private Float   price;
+    private String pic;
+    private Date createdate;
+    private String detail;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Items{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", pic='" + pic + '\'' +
+                ", createdate=" + createdate +
+                ", detail='" + detail + '\'' +
+                '}';
+    }
+}
---
+++ maven_parent/maven_web/src/main/java/cn/itcast/controller/ItemsController.java
@@ -0,0 +1,26 @@
+package cn.itcast.controller;
+
+
+import cn.itcast.domain.Items;
+import cn.itcast.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/items")
+public class ItemsController {
+
+        @Autowired
+    private ItemsService itemsService;
+        @RequestMapping("/findDetail")
+        public String showItems(Model model){
+
+
+            Items items = itemsService.findById (1);
+            model.addAttribute ("items",items);
+
+            return "viewItems";
+        }
+}
